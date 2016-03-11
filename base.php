c72be187f8ee7a468f8958b49cae62fb15351b7c
Gabor Szabo <szabogabi@gmail.com>
2016-03-11 13:11:47 +0100

    bugfix

--- base.php
+++ base.php
@@ -1,5 +1,14 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74996631-1', 'auto');
+  ga('send', 'pageview');
+</script>
 
   <!--[if lt IE 9]>
     <div class="alert alert-warning">
@@ -23,9 +32,6 @@
   <?php get_template_part('templates/footer'); ?>
 
   <?php wp_footer(); ?>
-
-
-
 
 </body>
 </html>
--- templates/content-project.php
+++ templates/content-project.php
@@ -2,16 +2,16 @@
   <article <?php post_class('project--single'); ?>>
   <div class="wrapper wrapper--wide">
     <section class="project__gallery">
-			
-			<?php 
+
+			<?php
 				$galleryfiles = get_post_meta( $post->ID , '_pdata_gallery', 1 );
 				if ($galleryfiles!='') {
 			?>
         <a href="#" class="fulltoggle"><i class="ion ion-arrow-expand"></i></a>
 				<div class="project__theslider master-slider ms-skin-default" id="project__theslider">
-					
+
           <?php if (get_post_meta( $post->ID , '_pdata_video', 1 )) :?>
-            <?php 
+            <?php
               $image_url_array = wp_get_attachment_image_src(get_post_meta( $post->ID , '_pdata_videoimg_id', 1 ), 'full', true);
               $image_url = $image_url_array[0];
               $thumb_url_array = wp_get_attachment_image_src(get_post_meta( $post->ID , '_pdata_videoimg_id', 1 ), 'tiny169', true);
@@ -28,7 +28,7 @@
           <?php endif; ?>
 
           <?php foreach ( (array) $galleryfiles as $attachment_id => $attachment_url ) { ?>
-						<?php 
+						<?php
 							$image_url_array = wp_get_attachment_image_src($attachment_id, 'full', true);
 							$image_url = $image_url_array[0];
 							$thumb_url_array = wp_get_attachment_image_src($attachment_id, 'tiny169', true);
@@ -43,7 +43,7 @@
 					<?php } ?>
 				</div>
 			<?php } else { the_post_thumbnail('large169'); } ?>
-		
+
 
 
 
@@ -60,18 +60,18 @@
         <p class="param__item"><span>Size:</span> <?php echo get_post_meta( $post->ID, '_pdata_size', true ); ?></p>
         <p class="param__item"><span>Client:</span> <?php echo get_post_meta( $post->ID, '_pdata_client', true ); ?></p>
         <p class="param__item"><span>Recognition:</span> <?php echo get_post_meta( $post->ID, '_pdata_recognition', true ); ?></p>
-        <p class="param__item"><span>Built:</span> <?php echo get_post_meta( $post->ID, '_pdata_built', true ); ?></p>        
+        <p class="param__item"><span>Built:</span> <?php echo get_post_meta( $post->ID, '_pdata_built', true ); ?></p>
       </section>
 
       <nav class="refnav">
         <hr>
         <div class="refnav__wrap">
-          <?php next_post_link('%link','<i class="ion ion-ios-arrow-left"></i><p class="navtext">%title</p>'); ?>
+          <?php next_post_link('%link','<i class="ion ion-ios-arrow-left"></i><span>Previous project</span><p class="navtext">%title</p>'); ?>
           <a href="<?php echo get_post_type_archive_link('project'); ?>" class="refnav__all">
             <i class="ion ion-grid"></i>
             <p class="navtext"><?php _e('Show All','nielstorp'); ?></p>
           </a>
-          <?php previous_post_link('%link','<p class="navtext">%title</p><i class="ion ion-ios-arrow-right"></i>'); ?>
+          <?php previous_post_link('%link','<p class="navtext">%title</p><span>Next project</span><i class="ion ion-ios-arrow-right"></i>'); ?>
         </div>
        </nav>
     </header>
@@ -88,8 +88,8 @@
 
 <footer class="project__related">
   <div class="wrapper wrapper--wide">
-    <?php 
-         
+    <?php
+
         yarpp_related(
           array(
             // Pool options: these determine the "pool" of entities which are considered
@@ -110,16 +110,16 @@
 
             // 'require_tax' => array(
             //     'projects' => 1
-            // ),  
+            // ),
 
             'threshold' => 5,
             'template' => 'yarpp-template-projectsquare.php',
             'limit' => 8, // maximum number of results
             'order' => 'score DESC'
           ),
-          $post->ID, 
+          $post->ID,
           true // (optional) true to echo the HTML block; false to return it
-        ); 
+        );
 
       ?>
   </div>
