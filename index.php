72233ea8b8bea61bf13f1a3278912ffd412c67b8
Szabó Gábor <szabogabi@gmail.com>
2020-02-11 14:08:49 +0100

    postcards squared

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php get_template_part('templates/header', 'blog'); ?>
 <div class="csuszat">
-  <div class="subtoggler">
-    <a href="#" class="subnav-toggle"><i class="ion ion-funnel"></i></a>
-    <span class="subtoggler__title"><?php _e('News','nt'); ?></span>
-  </div>
-  <?php if (!have_posts()) : ?>
+    <div class="subtoggler">
+        <a href="#" class="subnav-toggle"><i class="ion ion-funnel"></i></a>
+        <span class="subtoggler__title"><?php _e('News','nt'); ?></span>
+    </div>
+    <?php if (!have_posts()) : ?>
     <div class="wrapper wrapper--normal">
-      <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'nt'); ?>
-      </div>
+        <div class="alert alert-warning">
+            <?php _e('Sorry, no results were found.', 'nt'); ?>
+        </div>
     </div>
     <?php get_search_form(); ?>
-  <?php endif; ?>
-  <div class="wrapper wrapper--wide">
-    <div class="bloginner">
-      <?php while (have_posts()) : the_post(); ?>
-        <?php //get_template_part('templates/content', get_post_format()); ?>
-        <?php get_template_part('templates/content', 'squared'); ?>
-      <?php endwhile; ?>
+    <?php endif; ?>
+    <div class="wrapper wrapper--fullwidth">
+        <div class="bloginner">
+            <?php while (have_posts()) : the_post(); ?>
+            <?php //get_template_part('templates/content', get_post_format()); ?>
+            <?php get_template_part('templates/content', 'squared'); ?>
+            <?php endwhile; ?>
+        </div>
     </div>
-  </div>
 
-  <?php if ($wp_query->max_num_pages > 1) : ?>
+    <?php if ($wp_query->max_num_pages > 1) : ?>
     <nav class="post-nav">
-      <div class="wrapper wrapper--wide">
-        <ul class="pager">
-          <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'nt')); ?></li>
-          <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'nt')); ?></li>
-        </ul>
-      </div>
+        <div class="wrapper wrapper--wide">
+            <ul class="pager">
+                <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'nt')); ?></li>
+                <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'nt')); ?></li>
+            </ul>
+        </div>
     </nav>
-  <?php endif; ?>
+    <?php endif; ?>
 </div>
-
--- templates/content-squared.php
+++ templates/content-squared.php
@@ -1,34 +1,36 @@
 <article <?php post_class('post--squared'); ?>>
 
-<?php 
-  $smimage_url_array = wp_get_attachment_image_src(get_post_thumbnail_id(), 'small916', false);
-  $smimage_url = $smimage_url_array[0]; 
+    <?php
+  $smimage_url_array = wp_get_attachment_image_src(get_post_thumbnail_id(), 'small11', false);
+  $smimage_url = $smimage_url_array[0];
 ?>
-<style>
-  .post--squared.post-<?php echo get_the_ID(); ?> .post--squared__figure {
-    background-image: url('<?php echo $smimage_url; ?>');
-  }
-</style>
-	<figure class="post--squared__figure">
-		<a href="<?php the_permalink(); ?>">
-			<?php the_post_thumbnail('small169'); ?>
-		</a>
-    <?php if (get_post_meta( $post->ID , '_postdata_video', 1 )) :?>
-      <div class="archive__icons">
-        <a class="popup--video" href="<?php echo get_post_meta( $post->ID , '_postdata_video', 1 ); ?>">
-          <i class="ion ion-ios-videocam"></i>
-        </a>
-      </div>
-    <?php endif; ?>
-	</figure>
+    <style>
+    .post--squared.post-<?php echo get_the_ID();
 
-  <div class="post--squared__summary">
-    <header class="post--squared__header">
-      <?php get_template_part('templates/post-updated'); ?>
-      <h2 class="post--squared__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    </header>
-    <div class="post--squared__excerpt">
-		 <?php the_excerpt(); ?>
-	  </div>
-  </div>
+    ?>.post--squared__figure {
+        background-image: url('<?php echo $smimage_url; ?>');
+    }
+    </style>
+    <figure class="post--squared__figure">
+        <a href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail('small11'); ?>
+        </a>
+        <?php if (get_post_meta( $post->ID , '_postdata_video', 1 )) :?>
+        <div class="archive__icons">
+            <a class="popup--video" href="<?php echo get_post_meta( $post->ID , '_postdata_video', 1 ); ?>">
+                <i class="ion ion-ios-videocam"></i>
+            </a>
+        </div>
+        <?php endif; ?>
+    </figure>
+
+    <div class="post--squared__summary">
+        <header class="post--squared__header">
+            <?php get_template_part('templates/post-updated'); ?>
+            <h2 class="post--squared__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        </header>
+        <div class="post--squared__excerpt">
+            <?php the_excerpt(); ?>
+        </div>
+    </div>
 </article>
